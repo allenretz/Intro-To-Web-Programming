ca9e46a0ec805813106db6f537ca82051df67b4e
allenretz <aretzler222@gmail.com>
2018-03-07 00:18:25 -0600

    website update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,31 @@
 module.exports = function(grunt){
+
+    grunt.initConfig({
+        watch: {
+            html: {
+                files: ['*/index.html'],
+                tasks: ['newIndexPage'],
+            },
+        },
+    });
+       
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+            
+
+    grunt.registerTask('newIndexPage',function(){
+    
+        console.log('Updating The Directory Page');
+        //Todo Implement
+    });
+
+    grunt.registerTask('jsMin', ['uglify']); // Alies
+
+
+    grunt.registerTask('htmlTasks', ['newIndexPage'])
+    grunt.registerTask('jsTasks',   ['jsMin'])
+    grunt.registerTask('cssTasks' , ['newIndexPage'])
+    grunt.registerTask('default',  ['htmlTasks', 'watch']);
+
     
 };
