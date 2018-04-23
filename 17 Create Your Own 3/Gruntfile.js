39f7fead2cb7ce9d174ca3fcb1eca990e0371cd6
Allen Retzler <aretzler222@gmail.com>
2018-04-22 19:24:50 -0500

    Added Grunt files

---
+++ 17 Create Your Own 3/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        cssmin: {
+            target: {
+              files: [{
+                expand: true,
+                cwd: '.',
+                src: ['**/*.css', '!**/*.min.css'],
+                dest: '.',
+                ext: '.min.css'
+              }]
+            }
+          },
+	watch: {
+		scripts: {
+			files : ['**/*.css'],
+			tasks : ['css'],
+		}
+	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('css', ['cssmin']);
+    grunt.registerTask('default', ['css', 'watch']);
+
+};
+
+
+
+
